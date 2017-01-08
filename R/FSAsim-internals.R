40c6f21fd629403771932743385510dc9937f821
Derek Ogle <dogle@northland.edu>
2017-01-08 11:12:41 -0600

    Removed .onAttach

--- R/FSAsim-internals.R
+++ R/FSAsim-internals.R
@@ -4,28 +4,8 @@
 #'
 #' @rdname FSA-internals
 #' @keywords internal
-#' @aliases .onAttach getVarFromFormula iChk4Namespace iCheckRStudio
+#' @aliases getVarFromFormula iChk4Namespace iCheckRStudio
 #'
-
-##################################################################
-## Sends a start-up message to the console when the package is loaded.
-##################################################################
-.onAttach <- function(lib,pkg,...) {
-  ## Get version number -- basically code from readPkgVersion in SweaveListingUtils
-  vers <- read.dcf(system.file("DESCRIPTION",package=pkg,lib.loc=lib),fields="Version")
-  ## Send message
-  msg <- paste("\n\n")
-  msg <- paste(msg,"############################################\n")
-  msg <- paste(msg,"##","FSAsim package, version",vers,"         ##\n")
-  msg <- paste(msg,"##   by Derek H. Ogle, Northland College  ##\n")
-  msg <- paste(msg,"##                                        ##\n")
-  msg <- paste(msg,"## Type ?FSAsim for documentation.        ##\n")
-  msg <- paste(msg,"## Type citation('FSAsim') for citation   ##\n")
-  msg <- paste(msg,"##   please cite if used in publication.  ##\n")
-  msg <- paste(msg,"############################################\n\n")
-  packageStartupMessage(msg)
-}
-
 
 ##################################################################
 ### Get variable names from formulas 
